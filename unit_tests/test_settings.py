72df17e211e4918a0a4c28a86f3cfb2a758a293b
Eric Matthes <eric@Erics-MacBook-Pro.local>
2022-03-01 16:01:56 -0900

    Reuses same tmp project.

--- unit_tests/test_settings.py
+++ unit_tests/test_settings.py
@@ -7,8 +7,8 @@ from pathlib import Path
 import pytest
 
 
-@pytest.fixture
-def tmp_project(tmpdir):
+@pytest.fixture(scope='module')
+def tmp_project(tmpdir_factory):
     """Create a copy of the local sample project, and run simple_deploy
     against this project. Most tests will examine how the project
     was modified.
@@ -19,12 +19,13 @@ def tmp_project(tmpdir):
 
     # Root directory of local simple_deploy project.
     sd_root_dir = Path(__file__).parent.parent
-    cmd = f'sh setup_project.sh -d {tmpdir} -s {sd_root_dir}'
+    tmp_proj_dir = tmpdir_factory.mktemp('blog_project')
+    cmd = f'sh setup_project.sh -d {tmp_proj_dir} -s {sd_root_dir}'
     cmd_parts = cmd.split()
     subprocess.run(cmd_parts)
 
     # Return the location of the temp project.
-    return tmpdir
+    return tmp_proj_dir
 
 
 def test_creates_heroku_specific_settings_section(tmp_project):
@@ -32,6 +33,7 @@ def test_creates_heroku_specific_settings_section(tmp_project):
     settings_text = Path(tmp_project / 'blog/settings.py').read_text()
     assert "if 'ON_HEROKU' in os.environ:" in settings_text
 
+# @pytest.mark.skip()
 def test_imports_dj_database_url(tmp_project):    
     """Verify dj_database_url is imported."""
     settings_text = Path(tmp_project / 'blog/settings.py').read_text()
