35486378cb1a5d75360f5a416804d38f04b9a29d
Eric Matthes <eric@Erics-MacBook-Pro.local>
2022-03-01 15:42:23 -0900

    Two assertions in two test functions, but creates entire tmp project twice.

--- unit_tests/test_settings.py
+++ unit_tests/test_settings.py
@@ -4,14 +4,15 @@ import subprocess
 from time import sleep
 from pathlib import Path
 
-
-def run_command(cmd, use_shell=True):
-    """Run a command through subprocess."""
-    cmd_parts = cmd.split()
-    subprocess.run(cmd_parts)
+import pytest
 
 
-def test_tmp_dir(tmpdir):
+@pytest.fixture
+def tmp_project(tmpdir):
+    """Create a copy of the local sample project, and run simple_deploy
+    against this project. Most tests will examine how the project
+    was modified.
+    """
 
     # Pause, or the tmpdir won't be usable.
     sleep(0.2)
@@ -19,12 +20,19 @@ def test_tmp_dir(tmpdir):
     # Root directory of local simple_deploy project.
     sd_root_dir = Path(__file__).parent.parent
     cmd = f'sh setup_project.sh -d {tmpdir} -s {sd_root_dir}'
-    run_command(cmd)
+    cmd_parts = cmd.split()
+    subprocess.run(cmd_parts)
 
-    # Verify there's a Heroku-specific settings section.
-    settings_text = Path(tmpdir / 'blog/settings.py').read_text()
+    # Return the location of the temp project.
+    return tmpdir
+
+
+def test_creates_heroku_specific_settings_section(tmp_project):
+    """Verify there's a Heroku-specific settings section."""
+    settings_text = Path(tmp_project / 'blog/settings.py').read_text()
     assert "if 'ON_HEROKU' in os.environ:" in settings_text
 
-    # Verify correct database import.
-    settings_text = Path(tmpdir / 'blog/settings.py').read_text()
+def test_imports_dj_database_url(tmp_project):    
+    """Verify dj_database_url is imported."""
+    settings_text = Path(tmp_project / 'blog/settings.py').read_text()
     assert "import dj_database_url" in settings_text
