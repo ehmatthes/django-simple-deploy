56f7feb107f0d1918fd9d43cffcecdcf751238d2
Eric Matthes <eric@Erics-MacBook-Pro.local>
2022-03-01 15:33:23 -0900

    Makes two assertions about settings.

--- unit_tests/test_settings.py
+++ unit_tests/test_settings.py
@@ -5,23 +5,26 @@ from time import sleep
 from pathlib import Path
 
 
-def test_hello():
-    assert 2 == 2
-
 def run_command(cmd, use_shell=True):
     """Run a command through subprocess."""
     cmd_parts = cmd.split()
-    output = subprocess.run(cmd_parts, capture_output=True)
-    print(output)
+    subprocess.run(cmd_parts)
+
 
 def test_tmp_dir(tmpdir):
 
+    # Pause, or the tmpdir won't be usable.
     sleep(0.2)
 
+    # Root directory of local simple_deploy project.
     sd_root_dir = Path(__file__).parent.parent
-
     cmd = f'sh setup_project.sh -d {tmpdir} -s {sd_root_dir}'
     run_command(cmd)
 
-    print(tmpdir)
-    assert 0
+    # Verify there's a Heroku-specific settings section.
+    settings_text = Path(tmpdir / 'blog/settings.py').read_text()
+    assert "if 'ON_HEROKU' in os.environ:" in settings_text
+
+    # Verify correct database import.
+    settings_text = Path(tmpdir / 'blog/settings.py').read_text()
+    assert "import dj_database_url" in settings_text
