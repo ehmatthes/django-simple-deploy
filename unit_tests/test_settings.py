1c402cfa0c58a0f664bee64045f6b1964306176b
Eric Matthes <eric@Erics-MacBook-Pro.local>
2022-03-01 16:27:38 -0900

    Fixture for getting settings text.

--- unit_tests/test_settings.py
+++ unit_tests/test_settings.py
@@ -4,13 +4,16 @@ from pathlib import Path
 
 import pytest
 
+@pytest.fixture(scope='module')
+def settings_text(tmp_project):
+    return Path(tmp_project / 'blog/settings.py').read_text()
+    
 
-def test_creates_heroku_specific_settings_section(tmp_project):
+def test_creates_heroku_specific_settings_section(settings_text):
     """Verify there's a Heroku-specific settings section."""
-    settings_text = Path(tmp_project / 'blog/settings.py').read_text()
     assert "if 'ON_HEROKU' in os.environ:" in settings_text
 
-def test_imports_dj_database_url(tmp_project):    
+def test_imports_dj_database_url(settings_text):    
     """Verify dj_database_url is imported."""
-    settings_text = Path(tmp_project / 'blog/settings.py').read_text()
     assert "import dj_database_url" in settings_text
+
