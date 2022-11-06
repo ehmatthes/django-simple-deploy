5127d1ccbd1bacad7ece5899906a6e3ed1fe8ad8
Eric Matthes <eric@Erics-MacBook-Pro.local>
2022-11-06 08:10:04 -0800

    Commented line to fail all tests and show path to tmp_proj_dir.

--- unit_tests/conftest.py
+++ unit_tests/conftest.py
@@ -19,6 +19,12 @@ def tmp_project(tmpdir_factory):
     # Root directory of local simple_deploy project.
     sd_root_dir = Path(__file__).parent.parent
     tmp_proj_dir = tmpdir_factory.mktemp('blog_project')
+
+    # To see where pytest creates the tmp_proj_dir, uncomment the following line.
+    #   All tests will fail, but the AssertionError will show you the full path
+    #   to tmp_proj_dir.
+    # assert not tmp_proj_dir
+    
     cmd = f'sh setup_project.sh -d {tmp_proj_dir} -s {sd_root_dir}'
     cmd_parts = cmd.split()
     subprocess.run(cmd_parts)
