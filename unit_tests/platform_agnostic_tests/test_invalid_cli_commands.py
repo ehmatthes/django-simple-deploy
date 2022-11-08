df413e678a93d5dcf83b5fe6836a27e3683ac4e0
Eric Matthes <eric@Erics-MacBook-Pro.local>
2022-11-08 11:43:43 -0900

    Test for invalid platform name works, still has diagnostics.

--- unit_tests/platform_agnostic_tests/test_invalid_cli_commands.py
+++ unit_tests/platform_agnostic_tests/test_invalid_cli_commands.py
@@ -9,15 +9,61 @@ import pytest
 
 # --- Test modifications to settings.py ---
 
-def test_bare_call(tmp_project, capfd):
-    """Call simple_deploy with no arguments."""
-    arg_string = ""
-    # cmd = f"sh utils/call_simple_deploy_invalid.sh -d {tmp_project} -a {arg_string}"
-    cmd = f"sh utils/call_simple_deploy_invalid.sh -d {tmp_project}"
+# def test_bare_call(tmp_project, capfd):
+#     """Call simple_deploy with no arguments."""
+#     arg_string = ""
+#     # cmd = f"sh utils/call_simple_deploy_invalid.sh -d {tmp_project} -a {arg_string}"
+#     cmd = f"sh utils/call_simple_deploy_invalid.sh -d {tmp_project}"
+#     cmd_parts = cmd.split()
+#     subprocess.run(cmd_parts)
+#     captured = capfd.readouterr()
+
+#     assert "The --platform flag is required;" in captured.err
+#     assert "Please re-run the command with a --platform option specified." in captured.err
+#     assert "$ python manage.py simple_deploy --platform platform_sh" in captured.err
+
+# def test_invalid_platform_call(tmp_project, capfd):
+#     """Call simple_deploy with an invalid --platform argument."""
+#     arg_string = "--platform unsupported_platform_name"
+#     cmd = f"sh utils/call_simple_deploy_invalid.sh -d {tmp_project} -a {arg_string}"
+#     # cmd = f"sh utils/call_simple_deploy_invalid.sh -d {tmp_project}"
+#     cmd_parts = cmd.split()
+#     subprocess.run(cmd_parts)
+#     captured = capfd.readouterr()
+
+#     # assert not cmd
+#     assert "The platform unsupported_platform_name is not currently supported." in captured.err
+
+
+
+def test_invalid_platform_call(tmp_project, capfd):
+    """Call simple_deploy with an invalid --platform argument."""
+    invalid_sd_command = "python manage.py simple_deploy --unit-testing --platform unsupported_platform_name"
+
+    diagnostic_file = tmp_project / "diagnostics.txt"
+
+    # cmd = f'sh utils/call_simple_deploy_invalid.sh {tmp_project} "{invalid_sd_command}"'
+    # diagnostic_file.write_text(cmd)
+
+    # We have to be careful about how we split cmd before passing it to subprocess.run(),
+    #   because invalid_sd_command has spaces in it. If we simply call cmd.split(), the command
+    #   will be just "python
+    # So, we need to split the first part of the command, and then append invalid_sd_command.
+    #   This keeps invalid_sd_command as one argument.
+    cmd = f'sh utils/call_simple_deploy_invalid.sh {tmp_project}'
     cmd_parts = cmd.split()
+    cmd_parts.append(invalid_sd_command)
+    diagnostic_file.write_text(str(cmd_parts))
+
+
+
+
+
+
+
+    # cmd_parts = cmd.split()
     subprocess.run(cmd_parts)
     captured = capfd.readouterr()
 
-    assert "The --platform flag is required;" in captured.err
-    assert "Please re-run the command with a --platform option specified." in captured.err
-    assert "$ python manage.py simple_deploy --platform platform_sh" in captured.err
+    # assert not cmd
+    assert "The platform unsupported_platform_name is not currently supported." in captured.err
--- unit_tests/utils/call_simple_deploy_invalid.sh
+++ unit_tests/utils/call_simple_deploy_invalid.sh
@@ -2,13 +2,16 @@
 
 # Flags:
 # -d: full path to temp directory
-while getopts d: flag
-do
-    case "${flag}" in
-        d) tmp_dir=${OPTARG};;
-        # a) arg_string=${OPTARG};;
-    esac
-done
+# while getopts "d:c:" flag
+# do
+#     case "${flag}" in
+#         d) tmp_dir=${OPTARG};;
+#         c) invalid_sd_command="${OPTARG}";;
+#     esac
+# done
+
+tmp_dir="$1"
+invalid_sd_command="$2"
 
 # All remaining work is done in the temp dir.
 cd "$tmp_dir"
@@ -16,5 +19,12 @@ cd "$tmp_dir"
 # Activate existing venv.
 source b_env/bin/activate
 
-# python manage.py simple_deploy --unit-testing "$a"
-python manage.py simple_deploy --unit-testing
+echo "\n\ntmp_dir: $tmp_dir" >> "$tmp_dir/diagnostics.txt"
+echo "\n\ninvalid_sd_command: $invalid_sd_command" >> "$tmp_dir/diagnostics.txt"
+
+$invalid_sd_command
+# python manage.py simple_deploy --unit-testing "$arg_string"
+# python manage.py simple_deploy --unit-testing
+
+# my_cmd="python manage.py simple_deploy --unit-testing --platform unsupported_platform_name"
+# $my_cmd
