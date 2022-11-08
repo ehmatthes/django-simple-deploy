dcc3a86c31b2c4cd5c104fda2478d47583a9acaa
Eric Matthes <eric@Erics-MacBook-Pro.local>
2022-11-08 11:48:42 -0900

    Both tests work; diagnostics still there.

--- unit_tests/platform_agnostic_tests/test_invalid_cli_commands.py
+++ unit_tests/platform_agnostic_tests/test_invalid_cli_commands.py
@@ -9,18 +9,23 @@ import pytest
 
 # --- Test modifications to settings.py ---
 
-# def test_bare_call(tmp_project, capfd):
-#     """Call simple_deploy with no arguments."""
-#     arg_string = ""
-#     # cmd = f"sh utils/call_simple_deploy_invalid.sh -d {tmp_project} -a {arg_string}"
-#     cmd = f"sh utils/call_simple_deploy_invalid.sh -d {tmp_project}"
-#     cmd_parts = cmd.split()
-#     subprocess.run(cmd_parts)
-#     captured = capfd.readouterr()
+def test_bare_call(tmp_project, capfd):
+    """Call simple_deploy with no arguments."""
+    invalid_sd_command = "python manage.py simple_deploy --unit-testing"
+    
+    # cmd = f"sh utils/call_simple_deploy_invalid.sh {tmp_project}"
+    # cmd_parts = cmd.split()
+    cmd = f'sh utils/call_simple_deploy_invalid.sh {tmp_project}'
+    cmd_parts = cmd.split()
+    cmd_parts.append(invalid_sd_command)
 
-#     assert "The --platform flag is required;" in captured.err
-#     assert "Please re-run the command with a --platform option specified." in captured.err
-#     assert "$ python manage.py simple_deploy --platform platform_sh" in captured.err
+
+    subprocess.run(cmd_parts)
+    captured = capfd.readouterr()
+
+    assert "The --platform flag is required;" in captured.err
+    assert "Please re-run the command with a --platform option specified." in captured.err
+    assert "$ python manage.py simple_deploy --platform platform_sh" in captured.err
 
 
 
