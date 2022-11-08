4488767b9f7d163262ab3acb3191acf7b6c4e1e8
Eric Matthes <eric@Erics-MacBook-Pro.local>
2022-11-07 17:38:00 -0900

    Attempt at using minimal project works, but saves no meaningful time.

--- sample_project_minimal/mysite/mysite/settings.py
+++ sample_project_minimal/mysite/mysite/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps.
+
+    # Third party apps.
+
+    # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ unit_tests/platform_agnostic_tests/test_invalid_cli_commands.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import subprocess
+
+import pytest
+
+
+# --- Fixtures ---
+
+# @pytest.fixture(scope='module')
+# def run_simple_deploy(tmp_project):
+#     # Call simple_deploy here, so it can target this module's platform.
+#     # cmd = f"sh call_simple_deploy.sh -d {tmp_project} -p platform_sh"
+#     sd_root_dir = Path(__file__).parents[3]
+#     cmd = f"sh utils/call_simple_deploy.sh -d {tmp_project} -p platform_sh -s {sd_root_dir}"
+#     cmd_parts = cmd.split()
+#     subprocess.run(cmd_parts)
+
+# @pytest.fixture(scope='module')
+# def settings_text(tmp_project):
+#     return Path(tmp_project / 'blog/settings.py').read_text()
+
+
+# --- Test modifications to settings.py ---
+
+# def test_creates_platformsh_specific_settings_section(run_simple_deploy, settings_text):
+#     """Verify there's a Platform.sh-specific settings section."""
+#     # Read lines from platform.sh settings template, and make sure these
+#     # lines are in the settings file. Remove whitespace from the lines before
+#     # checking.
+
+#     # Root directory of local simple_deploy project.
+#     sd_root_dir = Path(__file__).parents[3]
+#     path = sd_root_dir / 'simple_deploy/templates/platformsh_settings.py'
+#     lines = path.read_text().splitlines()
+#     for expected_line in lines[4:]:
+#         assert expected_line.strip() in settings_text
+
+def test_minimal_project_setup(tmp_project):
+    assert tmp_project
