87e6d103e8450c93c5dcc88ac66bd84029038e17
Eric Matthes <eric@Erics-MacBook-Pro.local>
2022-11-12 23:23:14 -0900

    Static files work on Fly.io deployments.

--- simple_deploy/templates/flyio_settings.py
+++ simple_deploy/templates/flyio_settings.py
@@ -4,8 +4,21 @@
 # --- Settings for Fly.io. ---
 import os
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if os.environ.get("ON_FLYIO_SETUP") or os.environ.get("ON_FLYIO"):
+    # Static file configuration needs to take effect during the build process,
+    #   and when deployed.
+    # from https://whitenoise.evans.io/en/stable/#quickstart-for-django-apps
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+    i = MIDDLEWARE.index("django.middleware.security.SecurityMiddleware")
+    MIDDLEWARE.insert(i + 1,"whitenoise.middleware.WhiteNoiseMiddleware")
+
 if os.environ.get("ON_FLYIO"):
+    # These settings need to be in place during deployment, but not during
+    #   the setup process.
+    # The `dj_database_url.parse()` call causes the build to fail; other settings
+    #   here may not.
     import dj_database_url
     
     # Use secret, if set, to update DEBUG value.
@@ -26,10 +39,3 @@ if os.environ.get("ON_FLYIO"):
     # Prevent CSRF "Origin checking failed" issue.
     CSRF_TRUSTED_ORIGINS = ['https://{{ deployed_project_name }}.fly.dev']
 
-if os.environ.get("ON_FLYIO_SETUP") or os.environ.get("ON_FLYIO"):
-     # from https://whitenoise.evans.io/en/stable/#quickstart-for-django-apps
-     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-     STATIC_URL = '/static/'
-     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-     i = MIDDLEWARE.index("django.middleware.security.SecurityMiddleware")
-     MIDDLEWARE.insert(i + 1,"whitenoise.middleware.WhiteNoiseMiddleware")
