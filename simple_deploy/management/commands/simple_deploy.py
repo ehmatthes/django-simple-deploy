25aba2ff3226da2e37eb0827d364567f080e7d1c
Eric Matthes <eric@Erics-MacBook-Pro.local>
2022-11-22 15:20:57 -0900

    All CLI args defined in separate module.

--- simple_deploy/management/commands/simple_deploy.py
+++ simple_deploy/management/commands/simple_deploy.py
@@ -14,6 +14,8 @@ from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
 from . import deploy_messages as d_msgs
+from simple_deploy.management.commands.utils import write_file_from_template
+from .cli import SimpleDeployCLI
 
 
 class Command(BaseCommand):
@@ -26,50 +28,54 @@ class Command(BaseCommand):
 
         # --- Platform-agnostic arguments ---
 
-        parser.add_argument('--automate-all',
-            help="Automate all aspects of deployment?",
-            action='store_true')
+        # parser.add_argument('--automate-all',
+        #     help="Automate all aspects of deployment?",
+        #     action='store_true')
 
-        parser.add_argument('--platform', type=str,
-            help="Which platform do you want to deploy to?",
-            default='')
+        # parser.add_argument('--platform', type=str,
+        #     help="Which platform do you want to deploy to?",
+        #     default='')
 
-        # Allow users to skip logging.
-        parser.add_argument('--no-logging',
-            help="Do you want a record of simple_deploy's output?",
-            action='store_true')
+        # # Allow users to skip logging.
+        # parser.add_argument('--no-logging',
+        #     help="Do you want a record of simple_deploy's output?",
+        #     action='store_true')
 
-        # Allow users to use simple_deploy even with an unclean git status.
-        parser.add_argument('--ignore-unclean-git',
-            help="Run simple_deploy even with an unclean `git status` message.",
-            action='store_true')
+        # # Allow users to use simple_deploy even with an unclean git status.
+        # parser.add_argument('--ignore-unclean-git',
+        #     help="Run simple_deploy even with an unclean `git status` message.",
+        #     action='store_true')
 
-        # --- Platform.sh arguments ---
+        # # --- Platform.sh arguments ---
 
-        # Allow users to set the deployed project name. This is the name that
-        #   will be used by the platform, which may be different than the name
-        #   used in the `startproject` command. See the Platform.sh script
-        #   for use of this flag.
-        parser.add_argument('--deployed-project-name', type=str,
-            help="What name should the platform use for this project?\n(This is normally discovered automatically through inspection.)",
-            default='')
+        # # Allow users to set the deployed project name. This is the name that
+        # #   will be used by the platform, which may be different than the name
+        # #   used in the `startproject` command. See the Platform.sh script
+        # #   for use of this flag.
+        # parser.add_argument('--deployed-project-name', type=str,
+        #     help="What name should the platform use for this project?\n(This is normally discovered automatically through inspection.)",
+        #     default='')
 
-        # Allow users to specify the region for a project when using --automate-all.
-        parser.add_argument('--region', type=str,
-            help="Which region do you want to deploy to?",
-            default='us-3.platform.sh')
+        # # Allow users to specify the region for a project when using --automate-all.
+        # parser.add_argument('--region', type=str,
+        #     help="Which region do you want to deploy to?",
+        #     default='us-3.platform.sh')
 
-        # --- Developer arguments ---
+        # # --- Developer arguments ---
 
-        # If we're doing local unit testing, we need to avoid some network
-        #   calls.
-        parser.add_argument('--unit-testing',
-            help="Used for local unit testing, to avoid network calls.",
-            action='store_true')
+        # # If we're doing local unit testing, we need to avoid some network
+        # #   calls.
+        # parser.add_argument('--unit-testing',
+        #     help="Used for local unit testing, to avoid network calls.",
+        #     action='store_true')
 
-        parser.add_argument('--integration-testing',
-            help="Used for integration testing, to avoid confirmations.",
-            action='store_true')
+        # parser.add_argument('--integration-testing',
+        #     help="Used for integration testing, to avoid confirmations.",
+        #     action='store_true')
+
+        cli = SimpleDeployCLI(parser)
+
+
 
 
     def handle(self, *args, **options):
