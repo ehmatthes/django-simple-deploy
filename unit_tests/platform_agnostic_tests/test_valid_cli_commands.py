f08813335e37f320fef51a55f9408ec8a6a85668
Eric Matthes <eric@Erics-MacBook-Pro.local>
2023-04-28 15:39:01 -0800

    Removed unneeded function call in test_valid_commands.

--- unit_tests/platform_agnostic_tests/test_valid_cli_commands.py
+++ unit_tests/platform_agnostic_tests/test_valid_cli_commands.py
@@ -13,29 +13,6 @@ from ..utils import manage_sample_project as msp
 
 # --- Helper functions ---
 
-def make_valid_call(tmp_proj_dir, valid_sd_command):
-    """Make a valid call.
-    Returns:
-    - None
-    """
-
-    # We have to be careful about how we split cmd before passing it to subprocess.run(),
-    #   because valid_sd_command has spaces in it. If we simply call cmd.split(), the command
-    #   will be just "python
-    # So, we need to split the first part of the command, and then append valid_sd_command.
-    #   This keeps valid_sd_command as one argument.
-    # DEV: Naming inconsistency; the existing call_simple_deploy_invalid.sh actually
-    #   works for making valid test calls as well.
-    # unit_test_dir = Path(__file__).parent
-    # call_sd_invalid_path = unit_test_dir/ "utils/call_simple_deploy_invalid.sh"
-    # cmd = f"sh {call_sd_invalid_path} {tmp_proj_dir}"
-
-    # # cmd = f'sh utils/call_simple_deploy_invalid.sh {tmp_proj_dir}'
-    # cmd_parts = cmd.split()
-    # cmd_parts.append(valid_sd_command)
-    # subprocess.run(cmd_parts)
-    return msp.call_simple_deploy(tmp_proj_dir, valid_sd_command)
-
 
 # --- Test valid platform-agnostic simple_deploy calls ---
 
