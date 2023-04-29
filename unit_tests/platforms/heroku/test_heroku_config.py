7b7f61895c098dfed7e542cb5fa77b23c2572a0f
Eric Matthes <eric@Erics-MacBook-Pro.local>
2023-04-28 17:53:38 -0800

    All heroku tests pass.

--- unit_tests/platforms/heroku/test_heroku_config.py
+++ unit_tests/platforms/heroku/test_heroku_config.py
@@ -14,84 +14,84 @@ def test_settings(tmp_project):
     """Verify settings have been changed for heroku."""
     hf.check_reference_file(tmp_project, 'blog/settings.py', 'heroku')
 
-# def test_requirements_txt(tmp_project, pkg_manager):
-#     """Test that the requirements.txt file is correct."""
-#     if pkg_manager == "req_txt":
-#         hf.check_reference_file(tmp_project, 'requirements.txt', 'heroku')
-#     elif pkg_manager == "poetry":
-#         hf.check_reference_file(tmp_project, "requirements.txt", "heroku",
-#                 reference_filename="poetry.requirements.txt")
-#     elif pkg_manager == "pipenv":
-#         assert not Path("requirements.txt").exists()
+def test_requirements_txt(tmp_project, pkg_manager):
+    """Test that the requirements.txt file is correct."""
+    if pkg_manager == "req_txt":
+        hf.check_reference_file(tmp_project, 'requirements.txt', 'heroku')
+    elif pkg_manager == "poetry":
+        hf.check_reference_file(tmp_project, "requirements.txt", "heroku",
+                reference_filename="poetry.requirements.txt")
+    elif pkg_manager == "pipenv":
+        assert not Path("requirements.txt").exists()
 
-# def test_pipfile(tmp_project, pkg_manager):
-#     """Test that Pipfile is correct."""
-#     if pkg_manager in ("req_txt", "poetry"):
-#         assert not Path("Pipfile").exists()
-#     elif pkg_manager == "pipenv":
-#         hf.check_reference_file(tmp_project, "Pipfile", "heroku")
+def test_pipfile(tmp_project, pkg_manager):
+    """Test that Pipfile is correct."""
+    if pkg_manager in ("req_txt", "poetry"):
+        assert not Path("Pipfile").exists()
+    elif pkg_manager == "pipenv":
+        hf.check_reference_file(tmp_project, "Pipfile", "heroku")
 
-# def test_pyproject_toml(tmp_project, pkg_manager):
-#     """Test that pyproject.toml is correct."""
-#     if pkg_manager in ("req_txt", "pipenv"):
-#         assert not Path("pyproject.toml").exists()
-#     elif pkg_manager == "poetry":
-#         # The file should be unchanged from the original, but it should exist.
-#         hf.check_reference_file(tmp_project, "pyproject.toml", "heroku")
+def test_pyproject_toml(tmp_project, pkg_manager):
+    """Test that pyproject.toml is correct."""
+    if pkg_manager in ("req_txt", "pipenv"):
+        assert not Path("pyproject.toml").exists()
+    elif pkg_manager == "poetry":
+        # The file should be unchanged from the original, but it should exist.
+        hf.check_reference_file(tmp_project, "pyproject.toml", "heroku")
 
-# def test_gitignore(tmp_project):
-#     """Test that .gitignore has been modified correctly."""
-#     hf.check_reference_file(tmp_project, '.gitignore', 'heroku')
-
-
-# # --- Test Heroku-specific files ---
-
-# def test_generated_procfile(tmp_project):
-#     """Test that the generated Procfile is correct."""
-#     hf.check_reference_file(tmp_project, 'Procfile', 'heroku')
-
-# def test_static_placeholder(tmp_project):
-#     """Test that the static dir is present, with a placeholder.txt file."""
-#     hf.check_reference_file(tmp_project, 'static/placeholder.txt', 'heroku')
+def test_gitignore(tmp_project):
+    """Test that .gitignore has been modified correctly."""
+    hf.check_reference_file(tmp_project, '.gitignore', 'heroku')
 
 
-# # --- Test logs ---
+# --- Test Heroku-specific files ---
 
-# def test_log_dir(tmp_project):
-#     """Test that the log directory exists, and contains an appropriate log file."""
-#     log_path = Path(tmp_project / 'simple_deploy_logs')
-#     assert log_path.exists()
+def test_generated_procfile(tmp_project):
+    """Test that the generated Procfile is correct."""
+    hf.check_reference_file(tmp_project, 'Procfile', 'heroku')
 
-#     # There should be exactly two log files.
-#     log_files = sorted(log_path.glob('*'))
-#     log_filenames = [lf.name for lf in log_files]
-#     # Check for exactly the log files we expect to find.
-#     assert 'deployment_summary.html' in log_filenames
-#     # DEV: Add a regex text for a file like "simple_deploy_2022-07-09174245.log".
-#     assert len(log_files) == 2
-
-#     # Read log file.
-#     # DEV: Look for specific log file; not sure this log file is always the second one.
-#     #   We're looking for one similar to "simple_deploy_2022-07-09174245.log".
-#     log_file = log_files[1]
-#     log_file_text = log_file.read_text()
-
-#     # Spot check for opening log messages.
-#     assert "INFO: Logging run of `manage.py simple_deploy`..." in log_file_text
-#     assert "INFO: Configuring project for deployment to Heroku..." in log_file_text
-
-#     # Spot check for success messages.
-#     assert "INFO: --- Your project is now configured for deployment on Heroku. ---" in log_file_text
-#     assert "INFO: Or, you can visit https://sample-name-11894.herokuapp.com." in log_file_text
+def test_static_placeholder(tmp_project):
+    """Test that the static dir is present, with a placeholder.txt file."""
+    hf.check_reference_file(tmp_project, 'static/placeholder.txt', 'heroku')
 
 
-# # --- Test staticfile setup ---
+# --- Test logs ---
 
-# def test_one_static_file(tmp_project):
-#     """There should be exactly one file in static/."""
-#     static_path = tmp_project / 'static'
-#     static_dir_files = sorted(static_path.glob('*'))
-#     assert len(static_dir_files) == 1
+def test_log_dir(tmp_project):
+    """Test that the log directory exists, and contains an appropriate log file."""
+    log_path = Path(tmp_project / 'simple_deploy_logs')
+    assert log_path.exists()
+
+    # There should be exactly two log files.
+    log_files = sorted(log_path.glob('*'))
+    log_filenames = [lf.name for lf in log_files]
+    # Check for exactly the log files we expect to find.
+    assert 'deployment_summary.html' in log_filenames
+    # DEV: Add a regex text for a file like "simple_deploy_2022-07-09174245.log".
+    assert len(log_files) == 2
+
+    # Read log file.
+    # DEV: Look for specific log file; not sure this log file is always the second one.
+    #   We're looking for one similar to "simple_deploy_2022-07-09174245.log".
+    log_file = log_files[1]
+    log_file_text = log_file.read_text()
+
+    # Spot check for opening log messages.
+    assert "INFO: Logging run of `manage.py simple_deploy`..." in log_file_text
+    assert "INFO: Configuring project for deployment to Heroku..." in log_file_text
+
+    # Spot check for success messages.
+    assert "INFO: --- Your project is now configured for deployment on Heroku. ---" in log_file_text
+    assert "INFO: Or, you can visit https://sample-name-11894.herokuapp.com." in log_file_text
+
+
+# --- Test staticfile setup ---
+
+def test_one_static_file(tmp_project):
+    """There should be exactly one file in static/."""
+    static_path = tmp_project / 'static'
+    static_dir_files = sorted(static_path.glob('*'))
+    assert len(static_dir_files) == 1
 
 
 # # --- Test Heroku host already in ALLOWED_HOSTS ---
