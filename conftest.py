1b75ca4c14906461a6cc2f92a1231422dbe064c4
Eric Matthes <eric@Erics-MacBook-Pro.local>
2023-04-30 12:45:42 -0800

    Cleaned up root conftest.py.

--- conftest.py
+++ conftest.py
@@ -1,12 +1,14 @@
-"""Only run pytest from unit_tests/.
+"""Root conftest.py"""
 
-If it makes sense to call pytest at the project root, overwrite this file.
-"""
-
+import os
 import sys
 from pathlib import Path
 
+import pytest
 
+
+# Only run pytest from unit_tests/.
+#   If it makes sense to call pytest at the project root, overwrite this file.
 if Path.cwd().name != "unit_tests":
     print("To run unit tests, first cd into the unit_tests/ directory, and then run pytest.")
     sys.exit()
