29f830365549e78daf43eee396e277477388c8ae
Eric Matthes <eric@Erics-Mac-Studio.local>
2023-06-19 21:16:25 -0800

    CLI options defined for integration testing.

--- integration_tests/conftest.py
+++ integration_tests/conftest.py
@@ -2,6 +2,11 @@
 
 import sys
 
+import pytest
+
+
+# --- Validity check ---
+
 def check_valid_call():
     """Make sure the test call works for integration tests."""
     # Require -s flag.
@@ -27,3 +32,49 @@ def check_valid_call():
 if not check_valid_call():
     print("That is not a valid command for integration testing.")
     sys.exit()
+
+
+# --- CLI args ---
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--dep-man-approach",
+        action="store",
+        default="req_txt",
+        help="Approach for dependency management: defaults to 'req_txt'"
+    )
+    parser.addoption(
+        "--pypi",
+        action="store_true",
+        default=False,
+        help="Test the PyPI version of simple_deploy."
+    )
+    parser.addoption(
+        "--automate-all",
+        action="store_true",
+        default=False,
+        help="Test the `--automate-all` approach."
+    )
+    parser.addoption(
+        "--skip-confirmations",
+        action="store_true",
+        default=True,
+        help="Skip all confirmations"
+    )
+
+# Bundle these options into a single object.
+class CLIOption:
+    def __init__(self, dep_man_approach, pypi, automate_all, skip_confirmations):
+        self.dep_man_approach = dep_man_approach
+        self.pypi = pypi
+        self.automate_all = automate_all
+        self.skip_confirmations = skip_confirmations
+
+@pytest.fixture(scope='session')
+def cli_option(request):
+    return CLIOption(
+        dep_man_approach=request.config.getoption("--dep-man-approach"),
+        pypi=request.config.getoption("--pypi"),
+        automate_all=request.config.getoption("--automate-all"),
+        skip_confirmations=request.config.getoption("--skip-confirmations")
+    )
---
+++ integration_tests/platforms/platform_sh/test_deployment.py
@@ -0,0 +1,8 @@
+
+
+def test_deployment(cli_option):
+    """Test deployment to platform_sh."""
+    pass
+
+    print("cli_option:")
+    print(cli_option.__dict__)
