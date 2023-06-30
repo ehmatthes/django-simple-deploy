a76133377a67e6446ef6b9726ffdcefbce793381
Eric Matthes <eric@Erics-Mac-Studio.local>
2023-06-29 18:43:28 -0800

    Updated tests for Fly.io work except for destroy command, because bad app name.

---
+++ integration_tests/platforms/fly_io/flyio_helper_functions.py
@@ -0,0 +1,75 @@
+"""Helper functions specific to Fly.io."""
+
+import re, time
+
+from ...utils.it_helper_functions import make_sp_call
+
+
+def create_project():
+    """Create a project on Fly.io."""
+    print("\n\nCreating a project on Fly.io...")
+    output = make_sp_call(f"fly apps create --generate-name", capture_output=True).stdout.decode().strip()
+    print('create_project output:', output)
+
+    re_app_name = r'(New app created: )(.*)'
+    app_name = re.search(re_app_name, output).group(1)
+    print(f"  App name: {app_name}")
+
+    return app_name
+
+def commit_configuration_changes():
+    """Commit configuration changes made by simple_deploy."""
+    print("\n\nCommitting changes...")
+    make_sp_call("git add .")
+    make_sp_call("git commit -am 'Configured for deployment.'")
+
+def deploy_project():
+    """Make a non-automated deployment."""
+    # # Pause before making push, otherwise project resources may not be available.
+    # time.sleep(30)
+
+    print("Deploying to Fly.io...")
+    make_sp_call("fly deploy")
+
+    # Open project and get URL.
+    output = make_sp_call("fly open", capture_output=True).stdout.decode().strip()
+    print('fly open output:', output)
+
+    re_url = r'opening (http.*) \.\.\.'
+    project_url = re.search(re_url, output).group(1)
+    if 'https' not in project_url:
+        project_url = project_url.replace('http', 'https')
+
+    print(f"  Project URL: {project_url}")
+
+    return project_url
+
+def get_project_url_name():
+    """Get project URL and app name of a deployed project."""
+    output = make_sp_call("fly info", capture_output=True).stdout.decode().strip()
+
+    re_app_name = r'.*Hostname = (.*)\.fly\.dev'
+    app_name = re.search(re_app_name, output).group(1)
+
+    print(f"  Found app name: {app_name}")
+
+    # Build URL.
+    project_url = f"https://{app_name}.fly.dev"
+    print(f"  Project URL: {project_url}")
+
+    return project_url, app_name
+
+def destroy_project(app_name):
+    """Destroy the deployed project, and all remote resources."""
+    print("\nCleaning up:")
+    print("  Destroying Fly.io project...")
+    # make_sp_call(f"fly apps destroy -y {app_name}")
+    cmd = f"fly apps destroy -y {app_name}"
+    print("  destroy app command:", cmd)
+    make_sp_call(cmd)
+
+    print("  Destorying Fly.io database...")
+    # make_sp_call(f"fly apps destroy -y {app_name}-db")
+    cmd = f"fly apps destroy -y {app_name}-db"
+    print("  destroy db command:", cmd)
+    make_sp_call(cmd)
---
+++ integration_tests/platforms/fly_io/test_deployment.py
@@ -0,0 +1,65 @@
+import re, time
+
+import pytest
+
+from ...utils import it_helper_functions as it_utils
+from . import flyio_helper_functions as flyio_utils
+
+
+# --- Test functions ---
+
+# For normal test runs, skip this test.
+# When working on setup steps, skip other tests and run this one.
+#   This will force the tmp_project fixture to run, without doing a full deployment.
+@pytest.mark.skip
+def test_dummy(tmp_project):
+    """Helpful to have an empty test to run when testing setup steps."""
+    pass
+
+# Skip this test and enable test_dummy() to speed up testing of setup steps.
+# @pytest.mark.skip
+def test_flyio_deployment(tmp_project, cli_options):
+    """Test the full, live deployment process to Fly.io."""
+
+    print("\nTesting deployment to Fly.io using the following options:")
+    print(cli_options.__dict__)
+
+    python_cmd = it_utils.get_python_exe(tmp_project)
+
+    # Create a new project on the remote host, if not testing --automate-all.
+    if not cli_options.automate_all:
+        app_name = flyio_utils.create_project()
+
+    # Run simple_deploy against the test project.
+    it_utils.run_simple_deploy(python_cmd, 'fly_io', cli_options.automate_all)
+
+    # If testing Pipenv, lock after adding new packages.
+    if cli_options.pkg_manager == 'pipenv':
+        it_utils.make_sp_call(f"{python_cmd} -m pipenv lock")
+
+    # Get the deployed project's URL, and ID so we can destroy it later.
+    #   This also commits configuration changes and pushes the project
+    #   when testing the configuration-only workflow.
+    if cli_options.automate_all:
+        project_url, app_name = flyio_utils.get_project_url_name()
+    else:
+        flyio_utils.commit_configuration_changes()
+        project_url = flyio_utils.deploy_project()
+
+    # Test functionality of both deployed app, and local project.
+    #   We want to make sure the deployment works, but also make sure we haven't
+    #   affected functionality of the local project using the development server.
+    remote_functionality_passed = it_utils.check_deployed_app_functionality(python_cmd, project_url)
+    local_functionality_passed = it_utils.check_local_app_functionality(python_cmd)
+    it_utils.summarize_results(remote_functionality_passed, local_functionality_passed,
+            cli_options)
+
+    # Ask if the tester wants to destroy the remote project.
+    #   It is sometimes useful to keep the deployment active beyond the automated
+    #   test run.
+    if it_utils.confirm_destroy_project(cli_options):
+        flyio_utils.destroy_project(app_name)
+
+    # Make final assertions, so pytest results are meaningful.
+    assert remote_functionality_passed
+    assert local_functionality_passed
