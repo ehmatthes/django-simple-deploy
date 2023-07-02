d429a2e5bfa01cd7fada46c8dfae8df02556afbd
Eric Matthes <eric@Erics-Mac-Studio.local>
2023-07-01 20:59:53 -0800

    req_txt integration test passes for Heroku, tears down successfully.

---
+++ integration_tests/platforms/heroku/test_deployment.py
@@ -0,0 +1,63 @@
+import re, time
+
+import pytest
+
+from ...utils import it_helper_functions as it_utils
+from . import utils as platform_utils
+
+
+# --- Test functions ---
+
+# For normal test runs, skip this test.
+# When working on setup steps, skip other tests and run this one.
+#   This will force the tmp_project fixture to run, without doing a full deployment.
+@pytest.mark.skip
+def test_dummy(tmp_project, request):
+    """Helpful to have an empty test to run when testing setup steps."""
+    pass
+
+# Skip this test and enable test_dummy() to speed up testing of setup steps.
+# @pytest.mark.skip
+def test_heroku_deployment(tmp_project, cli_options, request):
+    """Test the full, live deployment process to Heroku."""
+
+    # Cache the platform name for teardown work.
+    request.config.cache.set("platform", "heroku")
+
+    print("\nTesting deployment to Heroku using the following options:")
+    print(cli_options.__dict__)
+
+    python_cmd = it_utils.get_python_exe(tmp_project)
+
+    # Create a new project on the remote host, if not testing --automate-all.
+    if not cli_options.automate_all:
+        platform_utils.create_project()
+
+    # Run simple_deploy against the test project.
+    it_utils.run_simple_deploy(python_cmd, 'heroku', cli_options.automate_all)
+
+    # If testing Pipenv, lock after adding new packages.
+    if cli_options.pkg_manager == 'pipenv':
+        it_utils.make_sp_call(f"{python_cmd} -m pipenv lock")
+
+    # Push the project if not using --automate-all.
+    if not cli_options.automate_all:
+        it_utils.commit_configuration_changes()
+        platform_utils.push_project()
+
+    # Get the deployed project's URL for testing, and get the app_name
+    #   for teardown work.
+    project_url, app_name = platform_utils.get_project_url_name()
+    request.config.cache.set("app_name", app_name)
+
+    # Test functionality of both deployed app, and local project.
+    #   We want to make sure the deployment works, but also make sure we haven't
+    #   affected functionality of the local project using the development server.
+    remote_functionality_passed = it_utils.check_deployed_app_functionality(python_cmd, project_url)
+    local_functionality_passed = it_utils.check_local_app_functionality(python_cmd)
+    it_utils.summarize_results(remote_functionality_passed, local_functionality_passed,
+            cli_options)
+
+    # Make final assertions, so pytest results are meaningful.
+    assert remote_functionality_passed
+    assert local_functionality_passed
---
+++ integration_tests/platforms/heroku/utils.py
@@ -0,0 +1,55 @@
+"""Helper functions specific to Heroku."""
+
+import re, time
+
+from ...utils.it_helper_functions import make_sp_call
+
+
+def create_project():
+    """Create a project on Heroku."""
+    print("\n\nCreating a project on Heroku...")
+    make_sp_call("heroku create")
+
+def push_project():
+    """Make a non-automated deployment."""
+    # Consider pausing before the deployment. Some platforms need a moment
+    #   for the newly-created resources to become fully available.
+    # time.sleep(30)
+
+    print("Deploying to Heroku...")
+    make_sp_call("git push heroku main")
+
+    print("  Migrating deployed project...")
+    make_sp_call("heroku run python manage.py migrate")
+
+    print("  Opening project...")
+    make_sp_call("heroku open")
+
+def get_project_url_name():
+    """Get project URL and app name of the deployed project.
+    """
+    output = make_sp_call("heroku apps:info", capture_output=True).stdout.decode().strip()
+
+    re_app_name = r'=== (.*)'
+    app_name = re.search(re_app_name, output).group(1).strip()
+
+    print(f"  Found app name: {app_name}")
+
+    # Get URL.
+    re_url = r'Web URL:.*(https:.*)'
+    project_url = re.search(re_url, output).group(1).strip()
+    print(f"  Project URL: {project_url}")
+
+    return project_url, app_name
+
+def destroy_project(request):
+    """Destroy the deployed project, and all remote resources."""
+    print("\nCleaning up:")
+
+    app_name = request.config.cache.get("app_name", None)
+    if not app_name:
+        print("  No app name found; can't destroy any remote resources.")
+        return None
+
+    print("  Destroying Heroku project...")
+    make_sp_call(f"heroku apps:destroy --app {app_name} --confirm {app_name}")
