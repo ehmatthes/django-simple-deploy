bafbd98115de088127e3cf4c64d073994687bcce
Eric <eric@example.com>
2023-12-06 06:09:22 -0900

    Raises error when running without authenticated Fly CLI.

--- simple_deploy/management/commands/fly_io/deploy_messages.py
+++ simple_deploy/management/commands/fly_io/deploy_messages.py
@@ -41,6 +41,13 @@ In order to deploy to Fly.io, you need to install the Fly.io CLI.
 After installing the CLI, you can run simple_deploy again.
 """
 
+cli_logged_out = """
+You are currently logged out of the Fly.io CLI. Please log in,
+  and then run simple_deploy again.
+You can log in from  the command line:
+  $ fly auth login
+"""
+
 no_project_name = """
 A suitable Fly.io app to deploy against could not be found.
 
