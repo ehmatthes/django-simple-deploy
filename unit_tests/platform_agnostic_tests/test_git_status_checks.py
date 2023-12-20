1108f7585a9e3af1ad6ae6badce563cf69b9c677
Eric Matthes <ehmatthes@gmail.com>
2023-12-19 23:19:27 -0900

    Helper function to add log dir and log file.

--- unit_tests/platform_agnostic_tests/test_git_status_checks.py
+++ unit_tests/platform_agnostic_tests/test_git_status_checks.py
@@ -1,4 +1,6 @@
 """Check the response to various states of the user's repository.
+
+Should be able to use any valid platform for these calls.
 """
 
 from pathlib import Path
@@ -12,6 +14,14 @@ from ..utils import manage_sample_project as msp
 # --- Fixtures ---
 
 # --- Helper functions ---
+
+def add_simple_deploy_logs(tmp_project):
+    """Add simple_deploy_logs/ dir, and a dummy log file with a single line."""
+    log_dir = tmp_project / "simple_deploy_logs"
+    log_dir.mkdir()
+
+    log_path = log_dir / "dummy_log.log"
+    log_path.write_text("Dummy log entry.")
 
 # --- Test against various valid and invalid states of user's project. ---
 
@@ -27,8 +37,6 @@ def test_clean_git_status(tmp_project, capfd):
 def test_unacceptable_settings_change(tmp_project, capfd):
     """Call simple_deploy after adding a line to settings.py."""
     path = tmp_project / "blog" / "settings.py"
-    assert path.exists()
-
     settings_text = path.read_text()
     new_text = "\n# Placeholder comment to create unacceptable git status."
     new_settings_text = settings_text + new_text
@@ -43,13 +51,7 @@ def test_unacceptable_settings_change(tmp_project, capfd):
     assert "SimpleDeployCommandError" in stderr
 
 def test_simple_deploy_logs_exists(tmp_project, capfd):
-    log_dir = tmp_project / "simple_deploy_logs"
-    log_dir.mkdir()
-    assert log_dir.exists()
-
-    log_path = log_dir / "dummy_log.log"
-    log_path.write_text("Dummy log entry.")
-    assert log_path.exists()
+    add_simple_deploy_logs(tmp_project)
 
     sd_command = "python manage.py simple_deploy --platform fly_io"
     stdout, stderr = msp.call_simple_deploy(tmp_project, sd_command)
