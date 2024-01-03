0969bf6cce6c7e412877b7be646ad341db092260
Eric Matthes <ehmatthes@gmail.com>
2024-01-03 08:48:48 -0900

    Fixed naming inconsistencies.

--- simple_deploy/management/commands/utils.py
+++ simple_deploy/management/commands/utils.py
@@ -392,11 +392,11 @@ def add_pipenv_pkg(pipfile_path, package, version):
     pipfile_text = pipfile_path.read_text()
 
     # Align comments. Align at column 30; take away name length, and version spec space.
-    tab_string = " " * (30 - len(package) - 5 - len(version))
+    # tab_string = " " * (30 - len(package) - 5 - len(version))
 
     # Write package name right after [packages]. For simple projects, this shouldn't
     # cause any issues.
-    new_pkg_string = f'[packages]\n{package} = "{version}"{tab_string}# Added by simple_deploy command.'
+    new_pkg_string = f'[packages]\n{package} = "{version}"'
     pipfile_text = pipfile_text.replace("[packages]", new_pkg_string)
 
     pipfile_path.write_text(pipfile_text)
