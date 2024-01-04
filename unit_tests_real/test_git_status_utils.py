77938a924558e64a789f4656c2394c73e3ebe861
Eric Matthes <ehmatthes@gmail.com>
2024-01-04 09:55:05 -0900

    Test for _clean_diff().

--- unit_tests_real/test_git_status_utils.py
+++ unit_tests_real/test_git_status_utils.py
@@ -22,8 +22,7 @@ def test_simple_git_status():
     assert sd_utils.check_status_output(status_output, diff_output)
 
 
-def test_ignore_sd_logs():
-    status_output = " M .gitignore"
+def test_diff_ignore_sd_logs():
     diff_output = dedent(
         """\
         diff --git a/.gitignore b/.gitignore
@@ -35,12 +34,11 @@ def test_ignore_sd_logs():
         +simple_deploy_logs/"""
     )
 
-    assert sd_utils.check_status_output(status_output, diff_output)
+    assert sd_utils._check_git_diff(diff_output)
 
 
 def test_diff_settings_sd_with_newline():
     """Make sure a newline change at end of file doesn't fail the git status check."""
-    status_output = " M .gitignore"
     diff_output = dedent(
         """\
         diff --git a/blog/settings.py b/blog/settings.py
@@ -55,4 +53,22 @@ def test_diff_settings_sd_with_newline():
         +LOGIN_URL = 'users:login'"""
     )
 
-    assert sd_utils.check_status_output(status_output, diff_output)
+    assert sd_utils._check_git_diff(diff_output)
+
+def test_clean_diff():
+    diff_output = dedent(
+        """\
+        diff --git a/blog/settings.py b/blog/settings.py
+        index 6d40136..6395c5a 100644
+        --- a/blog/settings.py
+        +++ b/blog/settings.py
+        @@ -39,0 +40 @@ INSTALLED_APPS = [
+        +    'simple_deploy',
+        @@ -134 +135 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+        -LOGIN_URL = 'users:login'
+        \\ No newline at end of file
+        +LOGIN_URL = 'users:login'"""
+    )
+
+    cleaned_diff = sd_utils._clean_diff(diff_output.splitlines())
+    assert cleaned_diff == ["+    'simple_deploy',"]
