96c8999cad12e4905468f393c5bbf59de184b007
Eric Matthes <ehmatthes@gmail.com>
2024-01-16 01:21:34 -0900

    Ran black.

--- tests/e2e_tests/conftest.py
+++ tests/e2e_tests/conftest.py
@@ -11,6 +11,7 @@ from .utils.it_helper_functions import confirm_destroy_project
 
 # --- Validity check ---
 
+
 def check_valid_call(config):
     """Make sure the test call is valid for current e2e tests.
 
@@ -26,13 +27,18 @@ def check_valid_call(config):
         return False
 
     # Make sure unit tests or integration tests aren't being run as well.
-    if "unit_tests" in " ".join(config.args) or "integration_tests" in " ".join(config.args):
+    if "unit_tests" in " ".join(config.args) or "integration_tests" in " ".join(
+        config.args
+    ):
         msg = "Unit and integration tests should be run separate from e2e tests."
         print(msg)
         return False
 
     # Verify that one specific platform has been requested.
-    num_platforms = sum(platform in ' '.join(config.args) for platform in ['platform_sh', 'fly_io', 'heroku'])
+    num_platforms = sum(
+        platform in " ".join(config.args)
+        for platform in ["platform_sh", "fly_io", "heroku"]
+    )
     if num_platforms == 1:
         return True
     else:
@@ -43,6 +49,7 @@ def check_valid_call(config):
     # Can't verify it was a valid call, so return False.
     return False
 
+
 def pytest_configure(config):
     if not check_valid_call(config):
         pytest.exit("Invalid command for e2e testing.")
@@ -50,31 +57,33 @@ def pytest_configure(config):
 
 # --- CLI args ---
 
+
 def pytest_addoption(parser):
     parser.addoption(
         "--pkg-manager",
         action="store",
         default="req_txt",
-        help="Approach for dependency management: defaults to 'req_txt'"
+        help="Approach for dependency management: defaults to 'req_txt'",
     )
     parser.addoption(
         "--pypi",
         action="store_true",
         default=False,
-        help="Test the PyPI version of simple_deploy."
+        help="Test the PyPI version of simple_deploy.",
     )
     parser.addoption(
         "--automate-all",
         action="store_true",
         default=False,
-        help="Test the `--automate-all` approach."
+        help="Test the `--automate-all` approach.",
     )
     parser.addoption(
         "--skip-confirmations",
         action="store_true",
         default=False,
-        help="Skip all confirmations"
+        help="Skip all confirmations",
     )
+
 
 # Bundle these options into a single object.
 class CLIOptions:
@@ -84,18 +93,21 @@ class CLIOptions:
         self.automate_all = automate_all
         self.skip_confirmations = skip_confirmations
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture(scope="session")
 def cli_options(request):
     return CLIOptions(
         pkg_manager=request.config.getoption("--pkg-manager"),
         pypi=request.config.getoption("--pypi"),
         automate_all=request.config.getoption("--automate-all"),
-        skip_confirmations=request.config.getoption("--skip-confirmations")
+        skip_confirmations=request.config.getoption("--skip-confirmations"),
     )
+
 
 # --- Sample project ---
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture(scope="session")
 def tmp_project(tmp_path_factory, pytestconfig, cli_options, request):
     """Create a copy of the local sample project, which will be deployed.
 
@@ -109,10 +121,10 @@ def tmp_project(tmp_path_factory, pytestconfig, cli_options, request):
 
     # Root directory of local simple_deploy project.
     sd_root_dir = Path(__file__).parent.parent.parent
-    tmp_proj_dir = tmp_path_factory.mktemp('blog_project')
+    tmp_proj_dir = tmp_path_factory.mktemp("blog_project")
 
     print(f"\nTemp project directory: {tmp_proj_dir}")
-    
+
     # Copy sample project to tmp dir, and set up the project for using simple_deploy.
     msp.setup_project(tmp_proj_dir, sd_root_dir, cli_options)
 
