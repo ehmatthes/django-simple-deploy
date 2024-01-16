b8417d6074b3d1f7419b33300a6dab863f77b928
Eric Matthes <ehmatthes@gmail.com>
2024-01-16 00:19:25 -0900

    Removed diagnostics.

--- tests/unit_tests_real/conftest.py
+++ tests/unit_tests_real/conftest.py
@@ -1,17 +1,1 @@
 """Conftest for actual unit tests."""
-
-# import sys
-
-
-# def pytest_configure(config):
-#     """check config."""
-#     # help(config)
-#     # print(config.__dict__)
-
-#     cli_options = vars(config.option)
-#     for k, v in cli_options.items():
-#         if v:
-#             print(k, v)
-
-#     sys.exit()
-
