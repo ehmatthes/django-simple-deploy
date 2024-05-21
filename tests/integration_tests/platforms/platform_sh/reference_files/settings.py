001c5918f651a1f2b898113b172d9eba8df1b764
Eric Matthes <ehmatthes@gmail.com>
2024-05-21 16:27:44 -0400

    Use double quotes in platformsh settings.

--- tests/integration_tests/platforms/platform_sh/reference_files/settings.py
+++ tests/integration_tests/platforms/platform_sh/reference_files/settings.py
@@ -142,29 +142,29 @@ if os.environ.get("PLATFORM_APPLICATION_NAME"):
 
     config = Config()
 
-    ALLOWED_HOSTS.append('*')
+    ALLOWED_HOSTS.append("*")
     DEBUG = False
 
-    STATIC_URL = '/static/'
+    STATIC_URL = "/static/"
 
     if config.appDir:
-        STATIC_ROOT = os.path.join(config.appDir, 'static')
+        STATIC_ROOT = os.path.join(config.appDir, "static")
     if config.projectEntropy:
         SECRET_KEY = config.projectEntropy
 
     if not config.in_build():
-        db_settings = config.credentials('database')
+        db_settings = config.credentials("database")
         DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.postgresql',
-                'NAME': db_settings['path'],
-                'USER': db_settings['username'],
-                'PASSWORD': db_settings['password'],
-                'HOST': db_settings['host'],
-                'PORT': db_settings['port'],
+            "default": {
+                "ENGINE": "django.db.backends.postgresql",
+                "NAME": db_settings["path"],
+                "USER": db_settings["username"],
+                "PASSWORD": db_settings["password"],
+                "HOST": db_settings["host"],
+                "PORT": db_settings["port"],
             },
-            'sqlite': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            "sqlite": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
             }
         }
