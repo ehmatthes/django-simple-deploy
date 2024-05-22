b88e72d7c1117b428e86a9eada97ab32ca25da0c
Eric Matthes <ehmatthes@gmail.com>
2024-05-22 19:50:29 -0400

    Simplified confirmation message about preliminary support for Platform.sh.

--- simple_deploy/management/commands/platform_sh/deploy_messages.py
+++ simple_deploy/management/commands/platform_sh/deploy_messages.py
@@ -8,16 +8,10 @@ from django.conf import settings
 
 
 confirm_preliminary = """
-***** Deployments to platform.sh are experimental at this point ***
+***** Deployment to platform.sh is under active development at this point ***
 
-- Support for deploying to platform.sh is in an exploratory phase at this point.
-- You should only be using this project to deploy to platform.sh at this point if
-  you are interested in helping to develop or test the simple_deploy project.
-- You should look at the deploy_platformsh.py script before running this command,
-  so you know what kinds of changes will be made to your project.
-- You should understand the platform.sh console, and be comfortable deleting resources
-  that are created during this deployment.
-- You may want to cancel this run and deploy to a different platform.
+- If you already have a project deployed on Platform.sh, configuration will fail.
+- If you have more than one org on Platform.sh, configuration will fail.
 """
 
 confirm_automate_all = """
