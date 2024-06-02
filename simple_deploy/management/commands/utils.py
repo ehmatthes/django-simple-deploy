bd1fe8161600aa09a17267b9be5af784272edd1a
Eric Matthes <ehmatthes@gmail.com>
2024-06-02 14:28:31 -0800

    Removed old code from write_file_from_template().

--- simple_deploy/management/commands/utils.py
+++ simple_deploy/management/commands/utils.py
@@ -16,42 +16,16 @@ import toml
 def write_file_from_template(dest_path, template_path, context=None):
     """Write a file based on a platform-specific template.
 
-    This may be a whole new file, such as a Dockerfile. Or, we may be
-    modifying an existing file such as settings.py.
+    This may be a whole new file, such as a Dockerfile. Or, we may be modifying an
+    existing file such as settings.py.
 
     Returns:
     - None
     """
-
-    # Get the platform name from the file that's importing this function.
-    #   This may need to be moved to its own file if it ends up being imported
-    #   from different places.
-    # caller = inspect.stack()[1].filename
-    # if sys.platform == "win32":
-    #     platform_re = r"\\simple_deploy\\management\\commands\\(.*)\\platform_deployer.py"
-    # else:
-    #     platform_re = r"/simple_deploy/management/commands/(.*)/platform_deployer.py"
-    # m = re.search(platform_re, caller)
-    # platform = m.group(1)
-
-    # Make a template engine that can access the platform's templates.
-    # my_dirs = get_app_template_dirs(f"management/commands/{platform}/templates")
-    # my_dirs = get_app_template_dirs(templates_path)
-
-    # my_engine = Engine(dirs=my_dirs)
     my_engine = Engine()
     template = my_engine.from_string(template_path.read_text())
     rendered_template = template.render(Context(context))
     dest_path.write_text(rendered_template)
-
-
-
-
-
-    # Generate the template string, and write it to the given path.
-    # template_string = my_engine.render_to_string(template, context)
-    # path.write_text(template_string)
-
 
 def get_numbered_choice(sd_command, prompt, valid_choices, quit_message):
     """Select from a numbered list of choices.
