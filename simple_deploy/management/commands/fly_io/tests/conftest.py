d0d2e62e8f4f634f72cc2c880c53193de59f0c8f
Eric Matthes <ehmatthes@gmail.com>
2024-06-03 20:30:39 -0800

    All tests pass, by ignoring integration tests in each plugin.

---
+++ simple_deploy/management/commands/fly_io/tests/conftest.py
@@ -0,0 +1,2 @@
+# Don't look at any test files in the sample_project/ dir.
+collect_ignore = ["integration_tests"]
---
+++ tests/integration_tests/test_plugins.py
@@ -0,0 +1,11 @@
+"""Coordinate integration for plugins that are installed by default."""
+
+import pytest
+
+default_plugins = ["fly_io"]
+
+def test_default_plugins():
+    """Test all default plugins."""
+
+    for plugin in default_plugins:
+        print(f"Testing plugin: {plugin}")
