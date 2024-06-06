0b4228468a9053cb01624a8987078d2da0c7b5a9
Eric Matthes <ehmatthes@gmail.com>
2024-06-06 06:38:05 -0800

    Moved fly e2e tests to plugin dir, and e2e conf validation to root conftest.

--- conftest.py
+++ conftest.py
@@ -2,6 +2,8 @@
 
 import sys
 
+import pytest
+
 
 # Don't look at any test files in the sample_project/ dir.
 collect_ignore = ["sample_project"]
@@ -14,3 +16,13 @@ if "e2e_tests" not in sys.argv:
 from pathlib import Path
 path = Path(__file__).parent / "tests" / "integration_tests" / "utils"
 sys.path.insert(0, path)
+
+
+# If running a plugin's e2e tests, run the e2e_tests conftest?
+running_e2e = any(["e2e_tests" in arg for arg in sys.argv])
+if running_e2e:
+    import tests.e2e_tests.conftest as e2e_conftest
+
+    def pytest_configure(config):
+        if not e2e_conftest.check_valid_call(config):
+            pytest.exit("Invalid command for e2e testing.")
