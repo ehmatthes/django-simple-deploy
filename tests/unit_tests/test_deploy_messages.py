a12b404d8d95174977f056c0646a16ebfe388407
Eric Matthes <ehmatthes@gmail.com>
2024-06-08 07:29:29 -0800

    First test for cross-platform deploy messages.

---
+++ tests/unit_tests/test_deploy_messages.py
@@ -0,0 +1,21 @@
+"""Tests for cross-platform deploy messages.
+
+These functions simply return strings. However, it can be easy to forget a
+return statement at the end of a dynamic function, and it can be easy to get 
+inconsistent spacing in the messages that are returned.
+
+A test here should mean that the spacing has been verified as appropriate in an actual
+run of simple_deploy. For example make a run with automate_all, cancel the run, and make
+sure the spacing of the rendered message looks appropriate.
+"""
+
+import simple_deploy.management.commands.deploy_messages as messages
+
+
+# --- Static messages ---
+
+# Just test one static message for now.
+def test_cancel_automate_all():
+    msg = messages.cancel_automate_all
+    assert msg == "\nOkay, canceling this run. If you want to configure your project\nfor deployment, run simple_deploy again without the --automate-all flag.\n"
+
