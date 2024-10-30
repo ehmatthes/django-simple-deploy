47d128d586391addc1b63ffad0f44e76ef69a2d0
Eric Matthes <ehmatthes@gmail.com>
2024-10-29 22:13:55 -0400

    Note about validation, and remove unused code.

--- simple_deploy/management/commands/simple_deploy.py
+++ simple_deploy/management/commands/simple_deploy.py
@@ -132,7 +132,8 @@ class Command(BaseCommand):
         self._check_required_hooks(pm)
 
         self._confirm_automate_all(pm)
-        # sd_config = self._get_sd_config()
+
+        # Validate sd_config before handing responsiblity off to plugin.
         self.sd_config.validate()
         pm.hook.simple_deploy_deploy()
 
