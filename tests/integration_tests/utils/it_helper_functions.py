ccb619338b519046b4202f28e283101d5d1f2174
Eric Matthes <ehmatthes@gmail.com>
2024-11-04 15:38:19 -0500

    Integration test utility check_reference_file() finds plugin reference files.

--- tests/integration_tests/utils/it_helper_functions.py
+++ tests/integration_tests/utils/it_helper_functions.py
@@ -15,7 +15,7 @@ from textwrap import dedent
 import pytest
 
 
-def check_reference_file(tmp_proj_dir, filepath, platform, reference_filename=""):
+def check_reference_file(tmp_proj_dir, filepath, plugin_name="", reference_filename=""):
     """Check that the test version of the file matches the reference version
     of the file.
 
@@ -46,12 +46,15 @@ def check_reference_file(tmp_proj_dir, filepath, platform, reference_filename=""
     # Root directory of local simple_deploy project.
     sd_root_dir = Path(__file__).parents[3]
     print("sd_root_dir:", sd_root_dir)
-    # fp_reference = sd_root_dir / f'integration_tests/platforms/{platform}/reference_files/{filename}'
-    fp_reference = (
-        sd_root_dir
-        / f"simple_deploy/management/commands/{platform}/tests/integration_tests/reference_files/{filename}"
-    )
-    assert fp_reference.exists()
+
+    # Only plugins use reference files for now. Assume plugin dir is in same directory as
+    # django-simple-deploy.
+    if plugin_name:
+        plugin_root_dir = sd_root_dir.parent / plugin_name
+        assert plugin_root_dir.exists()
+        
+        fp_reference = plugin_root_dir / f"tests/integration_tests/reference_files/{filename}"
+        assert fp_reference.exists()
 
     # The test file and reference file will always have different modified
     #   timestamps, so no need to use default shallow=True.
@@ -91,3 +94,4 @@ def check_package_manager_available(pkg_manager):
             msg += "https://pipenv.pypa.io/en/latest/install/#installing-pipenv"
 
         pytest.exit(msg)
+
