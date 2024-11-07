c12569d79f174c6616ad878678c6c6d316492d1e
Eric Matthes <ehmatthes@gmail.com>
2024-11-07 07:06:53 -0500

    Remove all unused code, remove unused pytest.ini.

--- conftest.py
+++ conftest.py
@@ -17,62 +17,28 @@ path = Path(__file__).parent / "tests" / "integration_tests" / "utils"
 sys.path.insert(0, str(path))
 
 
+# --- Dynamically import tests from all installed plugins ---
 
 # Get names of all plugins.
 plugin_names = packages_distributions().keys()
 plugin_names = [p for p in plugin_names if p.startswith("dsd_")]
 
-default_plugin_names = []#["dsd_flyio", "dsd_platformsh"]
-nd_plugin_names = [p for p in plugin_names if p not in default_plugin_names]
-
-nd_plugin_paths = []
-for nd_plugin_name in nd_plugin_names:
-    spec = importlib.util.find_spec(nd_plugin_name)
+plugin_paths = []
+for plugin_name in plugin_names:
+    spec = importlib.util.find_spec(plugin_name)
     path = Path(spec.origin).parents[1]
-    nd_plugin_paths.append(path)
+    plugin_paths.append(path)
 
 sd_root_dir = Path(__file__).parent
-nd_plugin_paths_rel = []
-for path in nd_plugin_paths:
+plugin_paths_rel = []
+for path in plugin_paths:
     path_rel = os.path.relpath(path, sd_root_dir)
     path_rel = Path(path_rel) / "tests"
-    nd_plugin_paths_rel.append(str(path_rel))
-
-# print(nd_plugin_paths)
-# print(nd_plugin_paths_rel)
+    plugin_paths_rel.append(str(path_rel))
 
 def pytest_configure(config):
-    # print("\n*** in config ***")
-    # print(config)
-    # config.args.append("../dsd-heroku/tests/")
-    # config.args.append("/Users/eric/projects/dsd-heroku/tests/")
-    # breakpoint()
-    # sys.exit()
-    for path in nd_plugin_paths_rel:
+    # Add plugin test paths to what's being collected.
+    # Don't add paths that have already been explicitly included.
+    for path in plugin_paths_rel:
         if path not in config.args:
             config.args.append(path)
-    # breakpoint()
-
-
-# for path in nd_plugin_paths_rel:
-# sys.exit()
-
-# def pytest_collection_modifyitems(config, items):
-#     # Convert relative paths to absolute paths based on root directory
-#     root_dir = Path(config.rootdir)
-#     absolute_plugin_paths = [root_dir / Path(p).resolve() for p in nd_plugin_paths_rel]
-
-#     # Filter and add items from these paths if they’re part of the collected items
-#     new_items = []
-#     for item in items:
-#         item_path = Path(item.fspath).resolve()
-#         if any(item_path.is_relative_to(abs_path) for abs_path in absolute_plugin_paths):
-#             new_items.append(item)
-
-#     # Add new items to the collection
-#     items.extend(new_items)
-
-#     # Optional: Print the added items for verification
-#     print("Dynamically added plugin tests:")
-#     for item in new_items:
-#         print(item.nodeid)
