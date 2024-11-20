159be24c76bbb9c75001bbacce59420854440fc1
Eric Matthes <ehmatthes@gmail.com>
2024-11-19 19:24:24 -0500

    Remove old code.

--- simple_deploy/management/commands/utils/sd_utils.py
+++ simple_deploy/management/commands/utils/sd_utils.py
@@ -226,44 +226,6 @@ def _clean_diff(diff_lines):
     return lines
 
 
-def _get_plugin_name_from_packages_old(platform, available_packages):
-    """Helper for getting correct plugin from platform name.
-
-    This is broken into a helper function to make testing easier.
-
-    Examples:
-    - The platform arg --fly_io will return dsd_flyio.
-    - The platform arg --digital_ocean will return dsd_digitalocean_<extension>.
-    """
-    # Remove underscores from platform arg.
-    platform_name = platform.replace("_", "")
-
-    # Get possible plugin names.
-    plugin_prefix = f"dsd_{platform_name}"
-    plugin_names = [
-        pkg_name for pkg_name in available_packages if plugin_prefix in pkg_name
-    ]
-    if len(plugin_names) == 0:
-        msg = f"Could not find plugin for the platform {platform}."
-        msg += "\n"
-        raise SimpleDeployCommandError(msg)
-
-    if len(plugin_names) == 1:
-        return plugin_names[0]
-
-    if len(plugin_names) == 2 and platform in ["fly_io", "platform_sh", "heroku"]:
-        # Return the third-party plugin. We're assuming a user who installed a
-        # third-party plugin overlapping a default plugin wants the custom plugin.
-        default_plugins = {"dsd_flyio", "dsd_platformsh", "dsd_heroku"}
-        return (set(plugin_names) - default_plugins).pop()
-
-    # At this point, it's simply an error if the user has installed multiple plugins
-    # targeting the same platform.
-    msg = f"There seem to be multiple plugins targeting the platform {platform}."
-    msg += "\nPlease uninstall redundant plugins."
-    msg += "\nFuture releases of simple_deploy may allow you to select which plugin to use."
-    raise SimpleDeployCommandError(msg)
-
 def _get_plugin_name_from_packages(available_packages):
     """Helper for getting plugin name from installed packages.
 
