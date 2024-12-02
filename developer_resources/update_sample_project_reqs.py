b93bbdfbc16181f972a587924ddf44011afaa78e
Eric Matthes <ehmatthes@gmail.com>
2024-12-02 08:42:27 -0500

    Remove unneeded import.

--- developer_resources/update_sample_project_reqs.py
+++ developer_resources/update_sample_project_reqs.py
@@ -10,7 +10,6 @@ Note that plugins will need to update some reference files after running this sc
 
 from pathlib import Path
 import subprocess
-import shlex
 
 # --- Update requirements.txt ---
 path_dsd_root = Path(__file__).parents[1]
